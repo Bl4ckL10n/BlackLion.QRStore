215aa91b8b6e2daf5d17e66543e3343b66116b33
Max Almonte <maxalmonte14@hotmail.com>
2022-06-13 18:09:45 -0400

    Validate URLs before saving or updating

---
+++ BlackLion.QRStore/BlackLion.QRStore/Helpers/URLValidatorHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BlackLion.QRStore.Helpers
+{
+    internal class URLValidatorHelper
+    {
+        public static bool IsValidURL(string URL)
+        {
+            string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            
+            return Rgx.IsMatch(URL);
+        }
+    }
+}

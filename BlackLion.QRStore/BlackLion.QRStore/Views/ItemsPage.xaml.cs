f2a76b6d003509bb9236c929702caed00522706e
Max Almonte <maxalmonte14@hotmail.com>
2022-06-13 13:48:27 -0400

    Add "no data yet" in list items view when the list is empty

--- BlackLion.QRStore/BlackLion.QRStore/Views/ItemsPage.xaml.cs
+++ BlackLion.QRStore/BlackLion.QRStore/Views/ItemsPage.xaml.cs
@@ -1,14 +1,5 @@
-﻿using BlackLion.QRStore.Models;
-using BlackLion.QRStore.ViewModels;
-using BlackLion.QRStore.Views;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlackLion.QRStore.ViewModels;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace BlackLion.QRStore.Views
 {

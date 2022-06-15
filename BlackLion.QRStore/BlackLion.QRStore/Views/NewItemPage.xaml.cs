77bf9a5d2336bc7844c3f2190bb26e6cb9e93856
Max Almonte <maxalmonte14@hotmail.com>
2022-06-14 20:42:57 -0400

    Scape URL before passing it to add new item view

--- BlackLion.QRStore/BlackLion.QRStore/Views/NewItemPage.xaml.cs
+++ BlackLion.QRStore/BlackLion.QRStore/Views/NewItemPage.xaml.cs
@@ -1,14 +1,9 @@
-﻿using BlackLion.QRStore.Models;
-using BlackLion.QRStore.ViewModels;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace BlackLion.QRStore.Views
 {
     public partial class NewItemPage : ContentPage
     {
-        //public string Domain { get; set; }
-        public Item Item { get; set; }
-
         public NewItemPage()
         {
             InitializeComponent();

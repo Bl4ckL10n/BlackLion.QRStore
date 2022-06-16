f6ebaf44e79c4ae5701a18a674aa390a51a4e18a
Max Almonte <maxalmonte14@hotmail.com>
2022-06-16 17:30:02 -0400

    Move initializing options to code-behind.

--- BlackLion.QRStore/BlackLion.QRStore/Views/ScanQRCodePage.xaml.cs
+++ BlackLion.QRStore/BlackLion.QRStore/Views/ScanQRCodePage.xaml.cs
@@ -1,14 +1,21 @@
-﻿using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Mobile;
 
 namespace BlackLion.QRStore.Views
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ScanQRCodePage : ContentPage
     {
         public ScanQRCodePage()
         {
             InitializeComponent();
+
+            var options = new MobileBarcodeScanningOptions();
+            options.PossibleFormats = new List<BarcodeFormat>() {
+                BarcodeFormat.QR_CODE
+            };
+            QRScanner.Options = options;
         }
 
         private void OnFlashButtonClicked(object sender, System.EventArgs e)

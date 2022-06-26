02554fa0001386148d8a47b03feed3a134f2ebef
Max Almonte <maxalmonte14@hotmail.com>
2022-06-25 21:39:15 -0400

    Disable debugging

--- BlackLion.QRStore/BlackLion.QRStore/AssemblyInfo.cs
+++ BlackLion.QRStore/BlackLion.QRStore/AssemblyInfo.cs
@@ -1,4 +1,10 @@
+using Android.App;
 using Xamarin.Forms.Xaml;
 
+#if DEBUG
+[assembly: Application(Debuggable=true)]
+#else
+[assembly: Application(Debuggable = false)]
+#endif
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: System.Resources.NeutralResourcesLanguage("en-US")]
